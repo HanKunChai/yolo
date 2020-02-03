2236509c71a8752e82b862d708628cfaef97ec0b
xiaoxu1025 <1312759081@qq.com>
2020-02-03 12:30:08 +0800

    update yolo

--- exception.py
+++ exception.py
@@ -20,3 +20,11 @@ class ValueValidException(Exception):
 
     def __str__(self):
         return self._msg
+
+
+class ResourceException(Exception):
+    def __init__(self, msg):
+        self._msg = msg
+
+    def __str__(self):
+        return self._msg
--- model/model.py
+++ model/model.py
@@ -6,12 +6,13 @@ from keras.regularizers import l2
 from functools import reduce, wraps
 from exception import ValueEmptyException
 import config as cfg
-import keras.backend as K
+import tensorflow as tf
 
 # load cfg
 cell_size = cfg.CELL_SIZE
 classes_num = cfg.CLASSES_NUM
 b = cfg.B
+iou_threshold = cfg.IOU_THRESHOLD
 c = (classes_num + 2 * (4 + 1))
 output_size = cell_size * cell_size * c
 
@@ -87,4 +88,88 @@ def yolo_model(inputs, rate=.5, trainable=True):
 
 
 def output_reshape(x):
-    return K.reshape(x, (-1, cell_size, cell_size, c))
+    return tf.reshape(x, (-1, cell_size, cell_size, c))
+
+
+def model_eval(outputs, im_size, iou_threshold=.5):
+    """
+    模型输出
+    :param outputs:
+    :return:
+    """
+    # 边框回归值
+    # (1, 7, 7, 8)
+    bboxes = outputs[..., 0:8]
+    # 置信度
+    # (1, 7, 7, 2)
+    confidences = outputs[..., 8:10]
+    # 类别
+    # (1, 7, 7, 20)
+    classes = outputs[..., 10:]
+    # (1, 7, 7, 2, 1)
+    confidences = tf.expand_dims(confidences, axis=-1)
+    # (1, 7, 7, 1, 20)
+    classes = tf.expand_dims(classes, axis=-2)
+    # (1, 7, 7, 2, 20)
+    scores = confidences * classes
+    # (1, 7, 7, 2, 4)
+    bboxes = tf.reshape(bboxes, shape=(-1, cell_size, cell_size, b, 4))
+
+    # bboxes 回归
+    grid_x = tf.range(cell_size, dtype=tf.float32)
+    grid_y = tf.range(cell_size, dtype=tf.float32)
+    grid_x, grid_y = tf.meshgrid(grid_x, grid_y)
+    x_offset = tf.reshape(grid_x, (-1, 1))
+    y_offset = tf.reshape(grid_y, (-1, 1))
+    x_y_offset = tf.concat([x_offset, y_offset], axis=-1)
+    x_y_offset = tf.cast(tf.reshape(x_y_offset, [cell_size, cell_size, 1, 2]), tf.float32)
+
+    # (1, 7, 7, 2, 4)
+    bboxes_normal = tf.stack([
+        (bboxes[..., 0] + x_y_offset[..., 0]) / cell_size * im_size[0],
+        (bboxes[..., 1] + x_y_offset[..., 1]) / cell_size * im_size[1],
+        tf.square(bboxes[..., 2]) * im_size[0],
+        tf.square(bboxes[..., 3]) * im_size[1]
+    ], axis=-1)
+
+    max_boxes_tensor = tf.constant(20, dtype=tf.int32)
+    # (7 * 7 * 2, 4)
+    bboxes_normal = tf.reshape(bboxes_normal, (-1, 4))
+
+    scores = tf.reshape(scores, (-1, 20))
+    # 得到最大值所在的索引
+    argmax_scores = tf.argmax(scores, axis=-1)
+    # (7 * 7 * 2, 1)
+    class_box_scores = tf.gather(scores, argmax_scores, axis=-1)
+    class_box_scores = tf.squeeze(class_box_scores, axis=-1)
+    nms_index = tf.image.non_max_suppression(bboxes_normal, class_box_scores, max_boxes_tensor,
+                                             iou_threshold=iou_threshold)
+    bboxes_normal = tf.gather(bboxes_normal, nms_index)
+    scores = tf.gather(class_box_scores, nms_index)
+    classes = tf.gather(argmax_scores, nms_index)
+    return bboxes_normal, scores, classes
+
+
+# import numpy as np
+#
+# a = np.array([[0.1, 0.8]])
+# b = np.array([[0.1, 0.1, 0.3, 0.4, 0.1]])
+# a = a.reshape((1, 2, 1))
+# b = b.reshape((1, 1, 5))
+# c = a * b
+# print(c, c.shape)
+
+# import tensorflow as tf
+#
+# a = tf.Variable([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]])
+# index_a = tf.Variable([0])
+# #
+# # b = tf.Variable([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+# # index_b = tf.Variable([2, 4, 6, 8])
+#
+# with tf.Session() as sess:
+#     sess.run(tf.global_variables_initializer())
+#     output = sess.run(tf.gather(a, index_a, axis=-1))
+#
+#     print(output, output.shape)
+    # print(sess.run(tf.gather(b, index_b)))
